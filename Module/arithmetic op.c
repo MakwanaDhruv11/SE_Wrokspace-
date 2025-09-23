3870eb8a7b5636358c07eacb89b350929049f26f
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-09-23 19:25:55 +0530

    Add files via upload

---
+++ Module/arithmetic op.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("enter your value : ");
+    scanf("%d",&a);
+    
+    printf("enter your value : ");
+    scanf("%d",&b);
+    
+    printf("total of : %d",a+b);
+     printf("\n total of : %d",a-b);
+      printf("\n total of : %d",a*b);
+       printf("\n total of : %f",a/b);
+       
+       
+    return 0;
+}
