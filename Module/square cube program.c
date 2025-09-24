13eba57ce7cb7ca2dd692fea9f53ee86c745ae44
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-09-24 10:37:51 +0530

    Add files via upload

---
+++ Module/square cube program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int square, cube;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    square = num * num;
+    cube   = num * num * num;
+    
+    printf("Square of %d = %d\n", num, square);
+    printf("Cube of %d = %d\n", num, cube);
+
+    return 0;
+}
