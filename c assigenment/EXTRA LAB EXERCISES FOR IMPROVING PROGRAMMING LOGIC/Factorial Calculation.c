0629a87f74fade5221bdbd35133c81911234a38b
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-24 15:08:50 +0530

    Update Factorial Calculation.c

--- c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Factorial Calculation.c
+++ c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Factorial Calculation.c
@@ -30,3 +30,30 @@ int main()
 
     
 }
+
+//recursive method
+
+
+#include <stdio.h>
+
+int fact(int n) 
+{
+    if(n == 1)
+    return 1;
+    else
+    return n * fact(n-1);
+}
+
+
+int main()
+{
+    int n;
+
+    printf("Enter Any Number: ");
+    scanf("%d", &n);
+
+    printf("\nFactorial : %d", fact(n));
+
+    
+}
+
