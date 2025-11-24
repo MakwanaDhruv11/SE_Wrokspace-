7efe271331a0587b54bdc5b29ac296a54da9ea95
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-24 14:54:42 +0530

    Update Max and Min in Array.c

--- c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Max and Min in Array.c
+++ c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Max and Min in Array.c
@@ -36,8 +36,8 @@ int main()
 
     
 
-  for( int i = 0; i < 9; i++)  //row
-    {
+  for( int i = 0; i < 9; i++)  
+      {
         for( int j = 0; j < 9 - i; j++)
         {
             if(arr[j] > arr[j + 1])  
