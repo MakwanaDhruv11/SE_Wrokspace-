fd8e7fc8c53675819d27fa171e86bdd9010dac8c
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-24 15:15:06 +0530

    Update String Reversal.c

--- c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/String Reversal.c
+++ c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/String Reversal.c
@@ -1,31 +1,6 @@
 // Write a C program that takes a string as input and reverses it using a function.
 // Challenge: Write the program without using built-in string handling functions.
 
-// #include <stdio.h>
-// #include <conio.h>
-// #include <string.h>
-
-// int main()
-// {
-//     char str[100], rev[100];
-
-//     printf("Enter a string: ");
-//     scanf("%s", str);
-
-//     strcpy(rev, str);
-//     strrev(rev);
-
-//     if (strcmp(str, rev) == 0)
-//     {
-//         printf("string is a Palindrome string.");
-//     }
-//     else
-//     {
-//         printf("string is not a Palindrome string.");
-//     }
-// }
-
-//-----------------------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <conio.h>
