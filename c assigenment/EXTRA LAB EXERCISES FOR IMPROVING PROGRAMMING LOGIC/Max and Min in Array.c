0cdbf46a6c2381659b4e14a5b07bcae9462b19be
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-26 19:48:43 +0530

    Update Max and Min in Array.c

--- c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Max and Min in Array.c
+++ c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Max and Min in Array.c
@@ -3,59 +3,71 @@
 // Challenge: Extend the program to sort the array in ascending order.
 
 #include <stdio.h>
-#include <conio.h>
-int main()
-{
-    int arr[10], big = 0, small = 0,temp=0;
 
+int main() {
+    int arr[10];
+    int i, max, min;
 
-    for (int i = 0; i < 10; i++)
-    {
-        printf("enter number %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
-    big=arr[0];
-    for (int j = 0; j < 10; j++)
-    {
-        if (big < arr[j])
-        {
-            big = arr[j];
-        }
-    }
-    small=arr[0];
-    for (int k = 0; k < 10; k++)
-    {
-        if (small > arr[k])
-        {
-            small = arr[k];
-        }
-    }
-
-    printf("big number is %d", big);
-    printf("\nsmall number is %d", small);
+    printf("Enter 10 int:\n");
 
     
+    for (i = 0; i < 10; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
 
-  for( int i = 0; i < 9; i++)  
-      {
-        for( int j = 0; j < 9 - i; j++)
+    max = min = arr[0]; 
+
+    
+    for (i = 1; i < 10; i++) 
+    {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Max value = %d\n", max);
+    printf("Min value = %d\n", min);
+
+    return 0;
+}
+
+
+
+/-----------challenge program --------
+
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int i, j, temp;
+
+    printf("Enter 10 int:\n");
+
+ 
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for (i = 0; i < 10; i++) 
+    {
+        for (j = i + 1; j < 10; j++) 
         {
-            if(arr[j] > arr[j + 1])  
+            if (arr[i] > arr[j]) 
             {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;  
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 
-    printf("\nAscending Order: ");
-    for(int i = 0; i < 10; i++)  
-    {
+    printf("Array in ascending order:\n");
+    for (i = 0; i < 10; i++) {
         printf("%d ", arr[i]);
     }
-
-
 
     return 0;
 }
