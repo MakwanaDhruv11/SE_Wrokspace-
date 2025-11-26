00105c5be4a2795d24d87bfc99331ea20559b82b
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-26 19:23:05 +0530

    Update Multiplication Table.c

--- c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Multiplication Table.c
+++ c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/Multiplication Table.c
@@ -3,19 +3,44 @@
 // Challenge: Allow the user to input the range of the multiplication table (e.g., from 1 to N).
 
 #include <stdio.h>
-#include <conio.h>
-int main()
-{
-    int n, i, ans, k;
-    printf("enter number:-");
-    scanf("%d", &n);
-    printf("\nenter range:-");
-    scanf("%d", &k);
-    for (i = 0; i <= k; i++)
-    {
-        ans = n * i;
-        printf("%d x %d = %d\n", n, i, ans);
-    }
 
+int main() 
+{
+    int num;
+      
+    printf("Enter an int to print its mul table: ");
+    scanf("%d", &num);
+    
+    printf("Mul table for %d:\n", num);
+    
+    for (int i = 1; i <= 10; i++) 
+    {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+    
+    return 0;
+}
+
+/------------challenge program--------------
+
+#include <stdio.h>
+
+int main() 
+{
+    int num, r;
+    
+    printf("Enter an int to print its mul table: ");
+    scanf("%d", &num);
+    
+    printf("Enter the r for the mul table (10 for 1 to 10): ");
+    scanf("%d", &r);
+    
+    printf("Mul table for %d (from 1 to %d):\n", num, r);
+    
+    for (int i = 1; i <= r; i++) 
+    {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+    
     return 0;
 }
