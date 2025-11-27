df8cebb9620826a40325bb82ccd05596fcbcf8f5
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-27 18:47:24 +0530

    Update Armstrong Number.c

--- c assigenment/extra logic bulid programs/Armstrong Number.c
+++ c assigenment/extra logic bulid programs/Armstrong Number.c
@@ -1,44 +1,63 @@
-Write a C program that checks whether a given number is an Armstrong number or not (e.g.,
-153 = 1^3 + 5^3 + 3^3).
-• Challenge: Write a program to find all Armstrong numbers between 1 and 1000.
+//Write a C program that checks whether a given number is an Armstrong number or not (e.g.,
+//153 = 1^3 + 5^3 + 3^3).
+//• Challenge: Write a program to find all Armstrong numbers between 1 and 1000.
     
-    
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
 
-int main()
+int main() 
 {
+    int num, temp, rem, sum = 0;
 
-    int og,num,rem,p=0;
-    double sum=0.0;
-    
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    printf("enter your value : ");
-    scanf("%d",&og);
+    temp = num;
 
-    num=og;
-
-    while (num != 0)
+    while (temp > 0) 
     {
-        num = num / 10;
-        p++;
+        rem = temp % 10;
+        sum = sum + (rem * rem * rem); // cube of digit
+        temp = temp / 10;
     }
-    
-    num=og;
-    
-    while(num > 0)
-    {
-        rem=num % 10;
-        sum+=pow(rem,p);
-        num=num / 10;
-  
-    }
-    
 
-    if(sum==og)
-    printf("\nGiven number is armstrong");
+    if (sum == num)
+        printf("%d is an Armstrong number.\n", num);
     else
-    printf("\nGiven number is not armstrong");
+        printf("%d is not an Armstrong number.\n", num);
 
+    return 0;
+}
+
+/-----------------challenge program-------------
+    
+    
+
+#include <stdio.h>
+
+int main() 
+{
+    int num, temp, rem, sum;
+
+    printf("Armstrong numbers between 1 and 1000:\n");
+
+    for (num = 1; num <= 1000; num++) 
+    {
+        temp = num;
+        sum = 0;
+
+        while (temp > 0) 
+        {
+            rem = temp % 10;
+            sum = sum + (rem * rem * rem); // cube of digit
+            temp = temp / 10;
+        }
+
+        if (sum == num) 
+        {
+            printf("%d ", num);
+        }
+    }
+
+    printf("\n");
     return 0;
 }
