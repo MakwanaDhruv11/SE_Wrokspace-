e4fe01df65c1ee7aff31614dc252330d30deda71
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-27 18:51:47 +0530

    Update Number Guessing Game.c

--- c assigenment/extra logic bulid programs/Number Guessing Game.c
+++ c assigenment/extra logic bulid programs/Number Guessing Game.c
@@ -3,36 +3,89 @@
 // within a limited number of attempts.
 // Challenge: Provide hints to the user if the guessed number is too high or too low.
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 #include <time.h>
-int main()
+
+int main() 
 {
+    int secret, guess;
+    int attempts = 5;
+
     srand(time(0)); 
-    int num = (rand() % 10) + 1;
-    int num2;
-    printf("you have 50 try (range 1 to 100)");
-   //printf("%d",num);
-    for (int i = 1; i <=50; i++)
+    secret = (rand() % 100) + 1; // number between 1 and 100
+
+    printf("Guess the number (1 to 100)\n");
+    printf("You have %d attempts.\n", attempts);
+
+    while (attempts > 0) 
     {
-        printf("\nguess number:-");
-        scanf("%d", &num2);
-        if (num < num2)
+        printf("\nEnter your guess: ");
+        scanf("%d", &guess);
+
+        if (guess == secret) 
         {
-            printf("\nnumber is too high!!");
-            printf("\nyou have %d chance left",(50-i));
+            printf("Congratulations! You guessed the number!\n");
+            return 0;
         }
-        else if (num > num2)
-        {
-            printf("\nnumber is too small!!");
-            printf("\nyou have %d chance left",(50-i));
-        }
-        else
-        {
-            printf("\nyay your ans is right\n");
+
+        attempts--;
+        if (attempts == 0)
             break;
-        }
+
+        printf("Wrong guess! Attempts left: %d\n", attempts);
     }
+
+    printf("\nSorry! You ran out of attempts.\n");
+    printf("The correct number was: %d\n", secret);
 
     return 0;
 }
+
+
+/-------------challenge program-------------------
+
+
+    
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() 
+{
+    int secret, guess;
+    int attempts = 7;  // more attempts for hint version
+
+    srand(time(0));
+    secret = (rand() % 100) + 1; 
+
+    printf("Guess the number (1 to 100)\n");
+    printf("You have %d attempts.\n", attempts);
+
+    while (attempts > 0) 
+    {
+        printf("\nEnter your guess: ");
+        scanf("%d", &guess);
+
+        if (guess == secret) 
+        {
+            printf("Great! You guessed the correct number!\n");
+            return 0;
+        }
+        else if (guess > secret) 
+        {
+            printf("Your guess is TOO HIGH!\n");
+        }
+        else {
+            printf("Your guess is TOO LOW!\n");
+        }
+
+        attempts--;
+        printf("Attempts left: %d\n", attempts);
+    }
+
+    printf("\nGame Over! You could not guess the number.\n");
+    printf("The correct number was: %d\n", secret);
+
+    return 0;
+}
+
