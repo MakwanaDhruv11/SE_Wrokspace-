fd9ef512b72eabc65a2d51e9f57479d66dff6620
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-27 18:54:49 +0530

    Update Pascals Triangle.c

--- c assigenment/extra logic bulid programs/Pascals Triangle.c
+++ c assigenment/extra logic bulid programs/Pascals Triangle.c
@@ -1,24 +1,69 @@
-#include<stdio.h>
-#include<stdio.h>
+//Write a C program that generates Pascal’s Triangle up to N rows using loops.
+//• Challenge: Implement the same program using a recursive function.
+
+#include <stdio.h>
 
 int main() {
-    int n, i, j, k, num;
+    int n, i, j;
 
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
-        // spaces
-        for (k = 0; k < n - i - 1; k++) {
-            printf("  ");
+        int value = 1;
+
+        // Print spaces
+        for (j = 0; j < n - i - 1; j++) {
+            printf(" ");
         }
-        num = 1;
+
+        // Print values
         for (j = 0; j <= i; j++) {
-            printf("%4d", num);   
-            num = num * (i - j) / (j + 1);
+            printf("%d ", value);
+            value = value * (i - j) / (j + 1);
         }
         printf("\n");
     }
 
     return 0;
 }
+
+
+/-------------challenge program---------------
+
+
+#include <stdio.h>
+
+// Recursive function to compute nCr
+int nCr(int n, int r) 
+{
+    if (r == 0 || r == n)
+        return 1;
+    return nCr(n - 1, r - 1) + nCr(n - 1, r);
+}
+
+int main() 
+{
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) 
+    {
+
+        for (j = 0; j < n - i - 1; j++) 
+        {
+            printf(" ");
+        }
+
+        for (j = 0; j <= i; j++) 
+        {
+            printf("%d ", nCr(i, j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
