99edde68a3e85f90f6481ce0b615d957fe17cd82
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-11-27 09:46:06 +0530

    Update String Reversal.c

--- c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/String Reversal.c
+++ c assigenment/EXTRA LAB EXERCISES FOR IMPROVING PROGRAMMING LOGIC/String Reversal.c
@@ -3,29 +3,80 @@
 
 
 #include <stdio.h>
-#include <conio.h>
-int main()
+#include <string.h>
+
+void reverseString(char str[]) 
 {
-  char str[100],revstr[100];
-  int count =0,j;
+    int len = strlen(str);
+    int i, j;
+    char temp;
 
-  printf("enter string:- ");
-  scanf("%s",&str);
-
-  while (str[count] != '\0')
-  {
-    count++;    
-  }
-    
-  j = count-1;
-  for(int i = 0; i <=count; i++)
-  {
-    revstr[i] = str[j];
-    j--;
-  }
-  printf("%s", revstr);
-
-  return 0;
+    for (i = 0, j = len - 1; i < j; i++, j--) 
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
+int main() 
+{
+    char s[100];
+
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    reverseString(s);
+
+    printf("Reversed string: %s\n", s);
+
+    return 0;
+}
+
+
+
+
+
+/------------challenge program---------------
+
+#include <stdio.h>
+
+void reverseManual(char str[]) 
+{
+    int i = 0, j, len = 0;
+    char temp;
+
+    // Find length manually
+    while (str[len] != '\0') 
+    {
+        len++;
+    }
+
+    j = len - 1;
+
+    // Reverse manually
+    while (i < j) 
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+
+        i++;
+        j--;
+    }
+}
+
+int main() 
+{
+    char s[100];
+
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    reverseManual(s);
+
+    printf("Reversed string (manual): %s\n", s);
+
+    return 0;
+}
 
