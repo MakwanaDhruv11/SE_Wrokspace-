5513c7bb10ac6e90a575659eae043a1abad209c5
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-12-08 16:26:30 +0530

    Create oddeven.c

---
+++ Module/oddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    printf("Enter a value :");
+    scanf("%d",&a);
+    
+    if(a%2==0){
+        printf("even number");
+    }else{
+        printf("odd number");
+    }
+
+}    
+    
