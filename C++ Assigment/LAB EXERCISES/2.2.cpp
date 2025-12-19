7d2634436f378b530392c82181b3580e0367477f
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-12-19 16:40:35 +0530

    Create 2.2.cpp

---
+++ C++ Assigment/LAB EXERCISES/2.2.cpp
@@ -0,0 +1,29 @@
+// 2. Type Conversion
+// o Write a C++ program that performs both implicit and explicit type conversions and
+// prints the results.
+// o Objective: Practice type casting in C++.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 5;
+    double b = a; 
+
+    cout << "Implicit Type Conversion:" << endl;
+    cout << "Integer value (a): " << a << endl;
+    cout << "Converted to double (b): " << b << endl;
+
+
+    double x = 9.78;
+    int y = (int)x; 
+
+    cout << "\nExplicit Type Conversion:" << endl;
+    cout << "Double value (x): " << x << endl;
+    cout << "Converted to int (y): " << y << endl;
+
+   
+    return 0;
+}
