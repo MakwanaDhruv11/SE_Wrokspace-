213cbe3f2ca2cef79387dddf6cf60345a1657ea5
MakwanaDhruv11 <dhruvmakwana904@gmail.com>
2025-12-19 16:34:16 +0530

    Create 2.1.cpp

---
+++ C++ Assigment/LAB EXERCISES/2.1.cpp
@@ -0,0 +1,23 @@
+// Variables and Constants
+// o Write a C++ program that demonstrates the use of variables and constants. Create
+// variables of different data types and perform operations on them.
+// o Objective: Understand the difference between variables and constants.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    const int c = 20;
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+
+    cout << "a + b = " << a + b << endl;
+
+    return 0;
+}
